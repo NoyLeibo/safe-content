d0b6b0cd791e0d504c45bbc1fc1dd2deb40eec0a
Noam Saar <noamsaar11@gmail.com>
2023-11-17 14:33:46 +0200

    Add onInit() reset

---
+++ js/safe-content.controller.js
@@ -0,0 +1,10 @@
+'use strict'
+
+function onInit() {
+    const elSecretContentSection = document.querySelector('.secret-content-section')
+    const elLoginSection = document.querySelector('.login-section')
+
+    elSecretContentSection.style.display = 'none'
+    elLoginSection.style.display = 'block'
+}
+
