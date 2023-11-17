b3bbf78bb33acd5ee73d13571031ec6674303ec9
Noam Saar <noamsaar11@gmail.com>
2023-11-17 14:38:10 +0200

    Add createUsers()

---
+++ js/services/safe-content.service.js
@@ -0,0 +1,24 @@
+'use strict'
+
+function _createUsers() {
+    gUsers - loadFromStorage('usersDB')
+    if(gUsers && gUsers.length) return
+
+    gUsers = [
+        _createUser('A', 'A', true),
+        _createUser('J', 'J', false),
+        _createUser('P', 'P', false),
+    ]
+
+}
+
+function _createUser(username, password, isAdmin) {
+    return {
+        id: makeId(),
+        username,
+        password,
+        lastLoginTime: null,
+        isAdmin,
+    }
+}
+
