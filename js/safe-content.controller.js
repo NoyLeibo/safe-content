d8a86886a102c7c4b4fbc86734a4c8409ecb33e0
Noam Saar <noamsaar11@gmail.com>
2023-11-17 14:34:18 +0200

    Add onDoLogin()

--- js/safe-content.controller.js
+++ js/safe-content.controller.js
@@ -8,3 +8,22 @@ function onInit() {
     elLoginSection.style.display = 'block'
 }
 
+function onDoLogin() {
+    const elUserName = document.querySelector('.user-name')
+    const elPassword = document.querySelector('.password')
+
+    if (!elUserName.value || !elPassword.value) return
+
+    const logIn = doLogin(elUserName.value, elPassword.value)
+
+    elUserName.value = ''
+    elPassword.value = ''
+
+    if (!logIn) {
+        alert('No user name or password found')
+        return
+    }
+
+    renderSecretContent()
+}
+
