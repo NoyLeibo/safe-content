3e6281d5e7c98a862a58d7689cb95527cdbba74c
Noam Saar <noamsaar11@gmail.com>
2023-11-17 14:39:23 +0200

    Add storage.service.js

---
+++ js/services/storage.service.js
@@ -0,0 +1,15 @@
+'use strict'
+
+function saveToStorage(key, value) {
+    const strVal = JSON.stringify(value)
+    localStorage.setItem(key, strVal)
+}
+
+function loadFromStorage(key) {
+    var strVal = localStorage.getItem(key)
+    return JSON.parse(strVal)
+}
+
+function clearStorage() {
+    localStorage.clear()
+}
